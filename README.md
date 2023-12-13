5f28cbe6fb1ceaaa0694bd6731ae378c128f140f
“Paulo <paulo@audimo.com>
2023-12-13 16:23:24 -0300

    project updated

---
+++ README.md
@@ -0,0 +1,32 @@
+# Bitcoin Proof of Work Sample
+
+This is a simple project that shows how Bitcoin proof of work really works (PoW).
+
+## Requirements
+
+```
+python3 -m pip install requirements.txt
+```
+
+## How to run
+
+```
+python3 main.py
+```
+
+You will see something like this:
+
+```
+Difficulty Bits: 10
+Target: 113078212145816597093331040047546785012958969400039613319782796882727665664
+Success with nonce: 264
+Hash is: 000445a9d2689d8ecb1c85e5240a485cfda1fb3a16794c2d9fd22bc780a254d6
+```
+
+If you want real Bitcoin difficulty number, set `use_bitcoin_difficulty` to `True` (it will take hours or days to find).
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2023, Paulo Coutinho
---
+++ main.py
@@ -0,0 +1,48 @@
+import hashlib
+import sys
+import requests
+
+def get_current_bitcoin_difficulty():
+    # API URL to fetch the current Bitcoin difficulty
+    url = "https://blockchain.info/q/getdifficulty"
+
+    try:
+        response = requests.get(url)
+        difficulty = response.text
+        return float(difficulty)
+    except Exception as e:
+        return str(e)
+
+def proof_of_work(header, difficulty_bits):
+    # Calculate the difficulty target based on difficulty bits
+    target = 2 ** (256 - difficulty_bits)
+    print(f"Difficulty Bits: {difficulty_bits}")
+    print(f"Target: {target}")
+
+    for nonce in range(sys.maxsize):
+        # Concatenate the header with the nonce and generate the hash
+        input_data = f"{header}{nonce}".encode('utf-8')
+        hash_result = hashlib.sha256(input_data).hexdigest()
+
+        # Convert the hash to a large number to compare with the target
+        hash_int = int(hash_result, 16)
+
+        if hash_int < target:
+            print(f"Success with nonce: {nonce}")
+            print(f"Hash is: {hash_result}")
+            return (hash_result, nonce)
+
+    print("Failure after trying all nonces")
+    return (None, None)
+
+# Example
+header = "My test block with large content"
+
+use_bitcoin_difficulty = False
+
+if use_bitcoin_difficulty:
+    difficulty_bits = get_current_bitcoin_difficulty()
+else:
+    difficulty_bits = 10
+
+proof_of_work(header, difficulty_bits)
