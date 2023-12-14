0eb51e67f1bf70e2ea09b4f0cafacab38ad6f3d4
“Paulo <paulo@audimo.com>
2023-12-14 13:08:43 -0300

    added processing time info

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import hashlib
 import sys
+from datetime import datetime
+
 import requests
+
 
 def get_current_bitcoin_difficulty():
     # API URL to fetch the current Bitcoin difficulty
@@ -13,6 +16,7 @@ def get_current_bitcoin_difficulty():
     except Exception as e:
         return str(e)
 
+
 def proof_of_work(header, difficulty_bits):
     # Calculate the difficulty target based on difficulty bits
     target = 2 ** (256 - difficulty_bits)
@@ -21,7 +25,7 @@ def proof_of_work(header, difficulty_bits):
 
     for nonce in range(sys.maxsize):
         # Concatenate the header with the nonce and generate the hash
-        input_data = f"{header}{nonce}".encode('utf-8')
+        input_data = f"{header}{nonce}".encode("utf-8")
         hash_result = hashlib.sha256(input_data).hexdigest()
 
         # Convert the hash to a large number to compare with the target
@@ -35,6 +39,7 @@ def proof_of_work(header, difficulty_bits):
     print("Failure after trying all nonces")
     return (None, None)
 
+
 # Example
 header = "My test block with large content"
 
@@ -43,6 +48,16 @@ use_bitcoin_difficulty = False
 if use_bitcoin_difficulty:
     difficulty_bits = get_current_bitcoin_difficulty()
 else:
-    difficulty_bits = 10
+    difficulty_bits = 30
 
+start_time = datetime.now()
 proof_of_work(header, difficulty_bits)
+end_time = datetime.now()
+duration = end_time - start_time
+
+# Display the processing time in HH:MM:SS format
+hours, remainder = divmod(duration.seconds, 3600)
+minutes, seconds = divmod(remainder, 60)
+formatted_duration = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+print(f"Processing time: {formatted_duration}")
